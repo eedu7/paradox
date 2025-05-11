393937b95590d923d30457eb89dcba46c0e05daf
eedu7 <mueedahmad067@gmail.com>
2025-05-11 18:26:17 +0500

    Added the dropdown menu for displaying more links

--- src/modules/navbar/constants.ts
+++ src/modules/navbar/constants.ts
@@ -1,4 +1,9 @@
-export const navbarItems: { title: string; href: string }[] = [
+interface NavItems {
+    title: string;
+    href: string;
+}
+
+export const navbarItems: NavItems[] = [
     {
         title: "Home",
         href: "/",
@@ -27,4 +32,63 @@ export const navbarItems: { title: string; href: string }[] = [
         title: "Contact",
         href: "/contact",
     },
+];
+
+interface SubNavItems extends NavItems {
+    description: string;
+}
+
+export const subNavItems: SubNavItems[][] = [
+    [
+        // Media & Highlights
+        {
+            title: "Gallery",
+            href: "/media/gallery",
+            description: "Photo highlights from events, school activities, and milestones.",
+        },
+        {
+            title: "Articles",
+            href: "/media/articles",
+            description: "Stories, updates, and insights shared by our community and contributors.",
+        },
+    ],
+    // Education & Schools
+    [
+        {
+            title: "Model Schools",
+            href: "/education/model-schools",
+            description: "Information about exemplary schools setting standards in education.",
+        },
+        {
+            title: "Enrollment Campaign",
+            href: "/education/enrollment-campaign",
+            description: "Our efforts to bring every child into the classroom.",
+        },
+    ],
+    // Community
+    [
+        {
+            title: "Initiatives",
+            href: "/community/initiatives",
+            description: "Projects and programs driven by the community.",
+        },
+        {
+            title: "Complaints",
+            href: "/community/complaints",
+            description: "A platform to report concerns or issues.",
+        },
+        {
+            title: "Team",
+            href: "/community/team",
+            description: "Meet the people behind our mission and progress.",
+        },
+    ],
+    // Activities
+    [
+        {
+            title: "Sports",
+            href: "/activities/sports",
+            description: "Updates, events, and achievements in school sports and athletics.",
+        },
+    ],
 ];
--- src/modules/navbar/index.tsx
+++ src/modules/navbar/index.tsx
@@ -3,14 +3,16 @@
 import React from "react";
 import {
     NavigationMenu,
+    NavigationMenuContent,
     NavigationMenuItem,
     NavigationMenuLink,
     NavigationMenuList,
+    NavigationMenuTrigger,
     navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import { navbarItems } from "@/modules/navbar/constants";
+import { navbarItems, subNavItems } from "@/modules/navbar/constants";
 import { usePathname } from "next/navigation";
 
 export const Navbar = () => {
@@ -27,6 +29,36 @@ export const Navbar = () => {
                         isActive={href === pathname}
                     />
                 ))}
+                <NavigationMenuItem>
+                    <NavigationMenuTrigger
+                        className={cn(
+                            ["/media", "/community", "/activities", "/education"].some((path) =>
+                                pathname.startsWith(path),
+                            ) && "bg-accent",
+                        )}
+                    >
+                        More
+                    </NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                        <div className="grid w-[700px] grid-cols-4">
+                            {subNavItems.map((value, index) => (
+                                <div
+                                    className="flex w-44 flex-col gap-2"
+                                    key={index}
+                                >
+                                    {value.map((data) => (
+                                        <ListItem
+                                            href={data.href}
+                                            key={data.title}
+                                            title={data.title}
+                                            children={data.description}
+                                        />
+                                    ))}
+                                </div>
+                            ))}
+                        </div>
+                    </NavigationMenuContent>
+                </NavigationMenuItem>
             </NavigationMenuList>
         </NavigationMenu>
     );
@@ -41,14 +73,38 @@ interface NavbarItemProps {
 const NavbarItem = ({ title, href, isActive }: NavbarItemProps) => {
     return (
         <NavigationMenuItem>
-            <Link
-                href={href}
-                passHref
+            <NavigationMenuLink
+                asChild
+                className={cn(navigationMenuTriggerStyle(), isActive && "bg-accent text-accent-foreground")}
             >
-                <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), isActive && "bg-accent")}>
-                    {title}
-                </NavigationMenuLink>
-            </Link>
+                <Link href={href}>{title}</Link>
+            </NavigationMenuLink>
         </NavigationMenuItem>
+    );
+};
+
+interface ListItemProps {
+    href: string;
+    className?: string;
+    title: string;
+    children: React.ReactNode;
+    isActive?: boolean;
+}
+
+const ListItem = ({ href, className, title, children, isActive }: ListItemProps) => {
+    return (
+        <NavigationMenuLink asChild>
+            <a
+                href={href}
+                className={cn(
+                    "hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground block space-y-1 rounded-md p-3 leading-none no-underline transition-colors outline-none select-none",
+                    isActive && "bg-accent text-accent-foreground",
+                    className,
+                )}
+            >
+                <div className="text-sm leading-none font-medium">{title}</div>
+                <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">{children}</p>
+            </a>
+        </NavigationMenuLink>
     );
 };
