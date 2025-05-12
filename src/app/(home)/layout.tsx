40dfd5fa3838effaab67328060931a8c03cc5b40
eedu7 <mueedahmad067@gmail.com>
2025-05-12 08:53:47 +0500

    Modify the MinisterMsg

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -5,7 +5,7 @@ import { AppWindowIcon } from "lucide-react";
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex min-h-screen flex-col">
-            <header className="py-4. flex items-center justify-between px-2">
+            <header className="flex items-center justify-between px-2 py-4">
                 <div>
                     <AppWindowIcon />
                 </div>
--- src/modules/minister-msg/index.tsx
+++ src/modules/minister-msg/index.tsx
@@ -3,20 +3,19 @@ import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 export const MinisterMsg = () => {
     return (
-        <div className="mx-auto max-w-7xl">
-            <AspectRatio
-                ratio={16 / 9}
-                className="p-4"
-            >
-                <iframe
-                    className="h-full w-full rounded-lg"
-                    src="https://www.youtube.com/embed/7NrTVcRwpaI?si=ZViU0jHrxUzpKzcX"
-                    title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerPolicy="strict-origin-when-cross-origin"
-                    allowFullScreen
-                ></iframe>
-            </AspectRatio>
-        </div>
+        <section className="mx-auto max-w-7xl">
+            <div className="my-2 lg:mx-4 lg:my-8">
+                <AspectRatio ratio={16 / 9}>
+                    <iframe
+                        className="h-full w-full rounded-lg"
+                        src="https://www.youtube.com/embed/7NrTVcRwpaI?si=ZViU0jHrxUzpKzcX"
+                        title="YouTube video player"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowFullScreen
+                    ></iframe>
+                </AspectRatio>
+            </div>
+        </section>
     );
 };
