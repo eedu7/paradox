90f2eb6339e65e231c62368063bb0ca71ef0829a
eedu7 <mueedahmad067@gmail.com>
2025-05-13 11:03:18 +0500

    modify the about us

--- src/modules/about-us/index.tsx
+++ src/modules/about-us/index.tsx
@@ -13,36 +13,43 @@ const poppins = Poppins({
 
 export const AboutUs = () => {
     return (
-        <section className="mx-auto flex max-w-7xl items-center justify-center px-2 py-10 md:p-4">
-            <div className="my-8">
-                <Link href="/about">
-                    <Card className="rounded-none">
-                        <CardContent className="flex flex-col md:flex-row md:gap-x-4">
-                            <CardTitle className="hidden"></CardTitle>
-                            <div className="flex items-center justify-center md:flex-none">
-                                <Image
-                                    src={aboutUsData.imageSrc}
-                                    alt={aboutUsData.title}
-                                    className="h-[256px] w-[256px] lg:h-[384px] lg:w-[384px]"
-                                />
-                            </div>
-                            <div className="mt-4 flex flex-col md:gap-y-4">
-                                <h1
-                                    className={cn(
-                                        poppins.className,
-                                        "md:text-2xl lg:text-4xl",
-                                        "bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent",
-                                    )}
+        <section className="bg-gradient-to-r from-gray-700 to-black">
+            <div className="mx-auto flex max-w-7xl items-center justify-center px-2 md:p-4 md:py-10">
+                <Card className="rounded-none border-none bg-transparent shadow-none outline-none">
+                    <CardContent className="flex flex-col md:flex-row md:gap-x-4">
+                        <CardTitle className="hidden"></CardTitle>
+                        <div className="flex items-center justify-center md:flex-none">
+                            <Image
+                                src={aboutUsData.imageSrc}
+                                alt={aboutUsData.title}
+                                className="h-[256px] w-[256px] lg:h-[384px] lg:w-[384px]"
+                            />
+                        </div>
+                        <div className="mt-4 flex flex-col md:gap-y-4">
+                            <h1
+                                className={cn(
+                                    poppins.className,
+                                    "md:text-2xl lg:text-4xl",
+                                    "bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent",
+                                    "text-white",
+                                )}
+                            >
+                                {aboutUsData.title}
+                            </h1>
+                            <p className="text-xs tracking-wider text-white md:text-sm lg:text-base">
+                                {aboutUsData.description[0]}
+                            </p>
+                            <div className="flex justify-end">
+                                <Link
+                                    href="/about"
+                                    className="flex-end hover:text- text-indigo-500 underline underline-offset-4 hover:text-indigo-600"
                                 >
-                                    {aboutUsData.title}
-                                </h1>
-                                <p className="text-muted-foreground text-[8px] tracking-wider md:text-sm lg:text-base">
-                                    {aboutUsData.description[0]}
-                                </p>
+                                    Learn more
+                                </Link>
                             </div>
-                        </CardContent>
-                    </Card>
-                </Link>
+                        </div>
+                    </CardContent>
+                </Card>
             </div>
         </section>
     );
