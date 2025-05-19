9058ef782f2d7f4f701b8d30a956b5bd86fcc26d
eedu7 <mueedahmad067@gmail.com>
2025-05-19 05:25:36 +0500

    Added the scouts images

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,3 +1,15 @@
 import ChiefExecutiveOfficerDEABahawalpurImage from "../../public/assets/images/chief-executive-officer-dea-bahawalpur.jpg";
+import OurMissionImage from "../../public/assets/images/scouts/our-mission.webp";
+import VisionImage from "../../public/assets/images/scouts/vision.jpg";
+import PBSAObjectiveImage from "../../public/assets/images/scouts/pbsa-objective.jpg";
+import FirstChiefScoutOfPakistanImage from "../../public/assets/images/scouts/first-chief-scout-of-pakistan.jpg";
+import PrideOfPakistanImage from "../../public/assets/images/scouts/pride-of-pakistan.jpg";
 
-export { ChiefExecutiveOfficerDEABahawalpurImage };
+export {
+    ChiefExecutiveOfficerDEABahawalpurImage,
+    OurMissionImage,
+    VisionImage,
+    PBSAObjectiveImage,
+    FirstChiefScoutOfPakistanImage,
+    PrideOfPakistanImage,
+};
